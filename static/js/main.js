ca4554fc8220a4f953b488334377ff77354eba21
Angry Admin <95914973+AngrySysOps@users.noreply.github.com>
2025-04-07 15:58:50 +0100

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,2 @@
+const root = document.getElementById("root");
+root.innerHTML = '<h2 style="text-align:center;margin-top:50px;">Angry Admin Storage Calculator will appear here once React is built</h2>';
